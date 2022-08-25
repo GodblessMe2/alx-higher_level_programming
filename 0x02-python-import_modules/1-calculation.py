760c45c3791a9676eb50e898bba1adb45001b3bb
GodblessMe2 <oshindeinde@gmail.com>
2022-08-25 22:27:14 +0100

    Changes

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    """Print the sum of 10 and 5."""
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, add(a, b)))
-
-    """Print the sub of 10 and 5."""
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-
-    """Print the mul of 10 and 5."""
-    print("{} + {} = {}".format(a, b, mul(a, b)))
-
-    """Print the div of 10 and 5."""
-    print("{} + {} = {}".format(a, b, div(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
