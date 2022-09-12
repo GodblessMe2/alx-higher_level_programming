10fef781d07627674f4bb77de38d3a6bbd3b93fb
GodblessMe2 <oshindeinde@gmail.com>
2022-09-12 22:50:32 +0100

    Working on task 0 - 103

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    try:
+        num = fct(*args)
+        return num
+    except Exception as error:
+        import sys
+        print("Exception: {}".format(error), file=sys.stderr)
+    return None
