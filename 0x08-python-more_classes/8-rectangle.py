2ee8e627fa9b412b73eb9a3674857c8d13f4b2e1
GodblessMe2 <oshindeinde@gmail.com>
2022-09-19 17:53:16 +0100

    Changes on task 8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -11,16 +11,6 @@ class Rectangle:
     number_of_instances = 0
     print_symbol = '#'
 
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        if type(rect_1) is not Rectangle:
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if type(rect_2) is not Rectangle:
-            raise TypeError("rect_2 must be an integer of Rectangle")
-        if rect_1.area() >= rect_2.area():
-            return (rect_1)
-        return (rect_2)
-
     def __init__(self, width=0, height=0):
         """Initializing the rectangle
         Args:
@@ -70,6 +60,23 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return (0)
         return (self.height * 2) + (self.width * 2)
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Return the Rectangle with the greater area.
+        Args:
+            rect_1 (Rectangle): The first Rectangle.
+            rect_2 (Rectangle): The second Rectangle.
+        Raises:
+            TypeError: If either of rect_1 or rect_2 is not a Rectangle.
+        """
+        if type(rect_1) is not Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if type(rect_2) is not Rectangle:
+            raise TypeError("rect_2 must be an integer of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return (rect_1)
+        return (rect_2)
 
     def __str__(self):
         """Return printable string representative of the rectangle"""
