2de71ea8de50371cf4f0273753092d360177767e
GodblessMe2 <oshindeinde@gmail.com>
2022-09-19 12:33:22 +0100

    Worked on task 0 - 2 for the unit testing

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Write a function that divide all elements of a matrix"""
+
+
+def matrix_divided(matrix, div):
+    if type(matrix) is not list:
+        raise TypeError("matrix must be an matrix (list of lists) of integers/floats")
+    size = None
+    for i in matrix:
+        if type(i) is not list:
+            raise TypeError("matrix must be an matrix (list of lists) of integers/floats")
+        if size is None:
+            size = len(i)
+        elif size != len(i):
+            raise TypeError("Each row of the matrix must have the same size")
+        for x in i:
+            if type(x) is not int and type(x) is not float:
+                raise TypeError("matrix must be an matrix (list of lists) of integers/floats")
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a member")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    return[[round(i / div, 2) for i in l] for l in matrix]
---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Write a function that pints
+   my name <first name> <last name>
+"""
+
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints "My name is" followed by the first name and optional last name"""
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    print("My name is", first_name, last_name)
