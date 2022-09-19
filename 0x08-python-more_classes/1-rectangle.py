03be7df258b8d7f71c8fcb3fa836eca8ded66409
GodblessMe2 <oshindeinde@gmail.com>
2022-09-19 17:24:29 +0100

    Changes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,31 +14,31 @@ class Rectangle:
            width (int): width of a side of the new rectangle
            height (int): height of a side of the new rectangle
         """
-        self.height = height
         self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """getter for the private instance attribute width"""
-        return self.__width
+        """Get/set width of the rectangle"""
+        return (self.__width)
 
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
-            raise TypeError("width must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
         """Return the current height of the rectangle"""
-        return self.__height
+        return (self.__height)
 
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
-            raise TypeError("height must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
