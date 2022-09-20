5dedd4d833af303b556831d8e6eb36fbf8b0d620
GodblessMe2 <oshindeinde@gmail.com>
2022-09-20 15:03:45 +0100

    Pycodestyle

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,5 +3,7 @@
 
 
 class LockedClass:
-    """LockedClass that let the user dynamically create a new instance attribute"""
+    """LockedClass that let the user dynamically
+    create a new instance attribute
+    """
     __slots__ = ["first_name"]
