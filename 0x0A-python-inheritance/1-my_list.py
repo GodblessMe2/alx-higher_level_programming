04622938bc1472384cd589d3a763eebbf5891546
GodblessMe2 <oshindeinde@gmail.com>
2022-09-26 22:41:00 +0100

    correction

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,10 +6,7 @@
 
 class MyList(list):
     """A subclass of the derived class"""
-    def __init__(self):
-        """Initializes the object"""
-        super().__init__()
-
+    
     def print_sorted(self):
         """Print out the ascending sort list"""
         print(sorted(self))
