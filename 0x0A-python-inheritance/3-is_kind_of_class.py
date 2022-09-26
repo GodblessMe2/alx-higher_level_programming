f7e4a87a9dbbe587b7a197386004c74cb0d1235b
GodblessMe2 <oshindeinde@gmail.com>
2022-09-26 22:23:18 +0100

    add Comment

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""
+This module contains the function is_kind_of_class
+"""
+
+
 def is_kind_of_class(obj, a_class):
     return (isinstance(obj, a_class))
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This module contains the function inherits_from
+"""
+
+
 def inherits_from(obj, a_class):
     """Return True if the object is an instance of the class
        that is been inherit(directly or indirectly)
