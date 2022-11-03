c75c5a1cb2b39f4a47c155b955b26ccbe65b5aac
GodblessMe2 <oshindeinde@gmail.com>
2022-11-03 12:21:32 +0100

    CHANGES

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
 -- Lists all records of the table second_table of the database
 -- hbtn_0c_0 in your MySQL server.
-SELECT TOP `score`, `name`
+SELECT `score`, `name`
 FROM `second_table`
 ORDER BY `score` DESC;
