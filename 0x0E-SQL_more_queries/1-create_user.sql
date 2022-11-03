1e0ad3d9fa2a1b239ea12396c8cf0c103477c213
GodblessMe2 <oshindeinde@gmail.com>
2022-11-03 23:17:55 +0100

    Corection and tasks

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,9 @@
 -- Creates the MySQL server user user_0d_1.
 CREATE USER
-    IF NOT EXISTS `user_0d_1`@`localhost`
-    IDENTIFIER BY `user_0d_1_Pwd`
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
-    ON *.* TO `user_0d_1`@`localhost`
-    IDENTIFIER BY `user_0d_1_Pwd`;
+   ON *.*
+   TO 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
