0f215d527e049dd096714b8d4d5d946a4c271842
GodblessMe2 <oshindeinde@gmail.com>
2022-11-29 22:52:23 +0100

    Correction on task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
-let result = '';
 const number = Math.floor(process.argv[2]);
 if (isNaN(number)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < number; i++) {
-    for (let j = 0; j < number; j++) {
-      result += 'X';
-    }
-    result += '\n';
+    let result = '';
+    for (let j = 0; j < number; j++) result += 'X';
+    console.log(result);
   }
-  console.log(result);
 }
