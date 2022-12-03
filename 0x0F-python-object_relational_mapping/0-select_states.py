4ad92b2dd1b0069246987a1dbf81dd759c27dc42
GodblessMe2 <oshindeinde@gmail.com>
2022-12-03 22:45:03 +0100

    changes

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states`")
-    [print(id, state) for state, id in c.fetchall()]
+    [print(id, state) for id, state in c.fetchall()]
