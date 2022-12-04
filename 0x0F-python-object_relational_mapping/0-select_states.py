d2ae4f2aee67266e0230b5cdcd576e98b26c98af
GodblessMe2 <oshindeinde@gmail.com>
2022-12-04 11:16:01 +0100

    correction

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -14,11 +14,11 @@ if __name__ == '__main__':
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
-        port=8012,
+        port=3306,
         host='localhost')
     cursor = db.cursor()
     cursor.execute('SELECT * FROM states ORDER BY states.id ASC;')
 
     states = cursor.fetchall()
-    for state, id in states:
-        print(id, state)
+    for state in states:
+        print(state)
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,12 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306,
+        host='localhost')
     c = db.cursor()
     c.query("SELECT * FROM `states` ORDER BY `id`")
     [print(state) for state in c.fetchall() if state[1][0] == "N"]
