fdbf299afefff084e6b23db35db2fb0e8dbf64fe
GodblessMe2 <oshindeinde@gmail.com>
2022-12-21 00:11:35 +0100

    correction

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,11 +4,10 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    res = urllib.request
-    request = res.Request("https://intranet.hbtn.io/status")
-    with res.urlopen(request) as response:
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
-        print("\t- type: {}". format(type(body)))
+        print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
-        print("\t- utf-8 content: {}".format(body.decode("utf-8")))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
