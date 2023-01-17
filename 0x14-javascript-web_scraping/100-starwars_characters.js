2e7b7ca10829e1e01d4e1799c788eb8bb6d522d7
GodblessMe2 <oshindeinde@gmail.com>
2023-01-17 18:14:30 +0100

    correction

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/starships/' + process.argv[2];
-request(url, (error, response, body) {
+request(url, (error, response, body) => {
   if (!error) {
     const data = JSON.parse(body).characters;
     data.array.forEach(dataResponse => {
