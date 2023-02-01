8ca3bed0261fb425b0ac41706faae81f3b1a2c9b
GodblessMe2 <oshindeinde@gmail.com>
2023-02-01 15:19:43 +0100

    delete file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# Fixes bad PHP extension in the WordPress file 'wp-setting.php'
-
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g/var/www/html/wp-setting.php',
-  path    => '/usr/local/bin/:/bin/'
-}
